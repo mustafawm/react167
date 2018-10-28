089af8607f4042b84e5883557059f399086a315c
Mustafa <mustafawm@msn.com>
2018-10-28 12:55:12 +1100

    localstorage, func to useState runs when init render only

--- src/CounterButton.js
+++ src/CounterButton.js
@@ -1,7 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 export default function CounterButton() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(
+    () => Number(window.localStorage.getItem('count')) || 0,
+  );
+
+  useEffect(
+    () => {
+      window.localStorage.setItem('count', count);
+    },
+    [count],
+  );
 
   return <button onClick={() => setCount(count + 1)}>{count} clicks</button>;
 }
